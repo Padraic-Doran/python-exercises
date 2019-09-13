e6682d28cf1ab56b939851b769cbf1a94599e3d4
Padraic Doran <padraic.doran@gmail.com>
2019-09-13 09:59:22 -0500

    functions

--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -167,3 +167,36 @@ def normalize_name(x):
 
 
 print(normalize_name(' Ho$w@dy Dud@e&y'))
+
+# Write a function named cumsum that accepts a list of numbers and 
+# returns a list that is the cumulative sum of the numbers in the list.
+
+def cum_sum(lst):
+    total = 0
+    new_lst = []
+    for num in lst:
+        total += num
+        new_lst.append(total)
+    return new_lst
+
+print(cum_sum([1,2,3,4]))
+
+
+def convert24(str1):
+    # Checking if last two elements of time
+    # is AM and first two elements are 12
+    if str1[-2:] == "AM" and str1[:2] == "12":
+        return "00" + str1[2:-2]
+    # remove the AM
+    elif str1[-2:] == "AM":
+        return str1[:-2]
+    # Checking if last two elements of time
+    # is PM and first two elements are 12
+    elif str1[-2:] == "PM" and str1[:2] == "12":
+        return str1[:-2]
+    else:
+
+    # add 12 to hours and remove PM
+        return str(int(str1[:2]) + 12) + str1[2:8]
+
+print(convert24("08:05:45 PM"))
